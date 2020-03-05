cd0347b9cde9ff991e8373a713524787b02e8c92
sshmind <sashadevlnx@gmail.com>
2020-03-05 11:39:01 +0330

    create custom user model

--- Backend/python/kactos/kactos/settings.py
+++ Backend/python/kactos/kactos/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pdf.apps.PdfConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -103,6 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
